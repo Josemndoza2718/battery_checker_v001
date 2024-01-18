f09b447d3fb1774df0a8dd0a748f6b9c23216849
abelardo <abelardo@CL9A13.INVERVAL.LOCAL>
2024-01-18 16:57:20 -0400

    init

---
+++ android/app/src/main/kotlin/com/example/battery_checker_v01/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.battery_checker_v01
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:battery_checker_v01/presentation/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    );
+  }
+}
